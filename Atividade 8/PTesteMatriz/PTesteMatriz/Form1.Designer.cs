51ff05a3ca8a040d4f3cc000871c66e2f70adb55
Felipe Jorge <69210245+Felipe1208@users.noreply.github.com>
2024-05-13 23:33:55 -0300

    Add files via upload

---
+++ Atividade 8/PTesteMatriz/PTesteMatriz/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PTesteMatriz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExercicio1 = new System.Windows.Forms.Button();
+            this.btnExercicio2 = new System.Windows.Forms.Button();
+            this.btnExercicio3 = new System.Windows.Forms.Button();
+            this.btnExercicio4 = new System.Windows.Forms.Button();
+            this.btnExercicio5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExercicio1
+            // 
+            this.btnExercicio1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnExercicio1.Location = new System.Drawing.Point(119, 117);
+            this.btnExercicio1.Name = "btnExercicio1";
+            this.btnExercicio1.Size = new System.Drawing.Size(149, 61);
+            this.btnExercicio1.TabIndex = 0;
+            this.btnExercicio1.Text = "Exercício 1";
+            this.btnExercicio1.UseVisualStyleBackColor = true;
+            this.btnExercicio1.Click += new System.EventHandler(this.btnExercicio1_Click);
+            // 
+            // btnExercicio2
+            // 
+            this.btnExercicio2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnExercicio2.Location = new System.Drawing.Point(300, 117);
+            this.btnExercicio2.Name = "btnExercicio2";
+            this.btnExercicio2.Size = new System.Drawing.Size(149, 61);
+            this.btnExercicio2.TabIndex = 1;
+            this.btnExercicio2.Text = "Exercício 2";
+            this.btnExercicio2.UseVisualStyleBackColor = true;
+            this.btnExercicio2.Click += new System.EventHandler(this.btnExercicio2_Click);
+            // 
+            // btnExercicio3
+            // 
+            this.btnExercicio3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnExercicio3.Location = new System.Drawing.Point(479, 117);
+            this.btnExercicio3.Name = "btnExercicio3";
+            this.btnExercicio3.Size = new System.Drawing.Size(149, 61);
+            this.btnExercicio3.TabIndex = 2;
+            this.btnExercicio3.Text = "Exercício 3";
+            this.btnExercicio3.UseVisualStyleBackColor = true;
+            this.btnExercicio3.Click += new System.EventHandler(this.btnExercicio3_Click);
+            // 
+            // btnExercicio4
+            // 
+            this.btnExercicio4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnExercicio4.Location = new System.Drawing.Point(205, 209);
+            this.btnExercicio4.Name = "btnExercicio4";
+            this.btnExercicio4.Size = new System.Drawing.Size(149, 61);
+            this.btnExercicio4.TabIndex = 3;
+            this.btnExercicio4.Text = "Exercício 4";
+            this.btnExercicio4.UseVisualStyleBackColor = true;
+            // 
+            // btnExercicio5
+            // 
+            this.btnExercicio5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnExercicio5.Location = new System.Drawing.Point(400, 209);
+            this.btnExercicio5.Name = "btnExercicio5";
+            this.btnExercicio5.Size = new System.Drawing.Size(149, 61);
+            this.btnExercicio5.TabIndex = 4;
+            this.btnExercicio5.Text = "Exercício 5";
+            this.btnExercicio5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExercicio5);
+            this.Controls.Add(this.btnExercicio4);
+            this.Controls.Add(this.btnExercicio3);
+            this.Controls.Add(this.btnExercicio2);
+            this.Controls.Add(this.btnExercicio1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExercicio1;
+        private System.Windows.Forms.Button btnExercicio2;
+        private System.Windows.Forms.Button btnExercicio3;
+        private System.Windows.Forms.Button btnExercicio4;
+        private System.Windows.Forms.Button btnExercicio5;
+    }
+}
+
---
+++ Atividade 8/PTesteMatriz/PTesteMatriz/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace PTesteMatriz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExercicio1_Click(object sender, EventArgs e)
+        {
+            int[] vetor = new int[20];
+            string auxiliar;
+            for (int i = 0; i ‹ vetor.Length; i++) {
+                auxiliar = Interaction.InputBox($"Digite un número ((i + 1)°)", "Entrada de dados*);
+                if (!int.TryParse(auxiliar, out vetor(i])) {
+                    MessageBox.Show("Número inválido");
+                    i--;
+                }
+            }
+            Array.Reverse(vetor);
+            auxiliar = "";
+
+            foreach (int i in vetor) {
+                auxiliar += i + "\n";
+            }
+            MessageBox.Show(auxiliar);
+            
+        }
+        private void btnExercicio2_Click(object sender, EventArgs e)
+        {
+            ArrayList Lista = new ArrayList();
+            Lista.Add("Ana");
+            Lista.Add("André");
+            Lista.Add("Débora");
+            Lista.Add("Fátima");
+            Lista.Add("João");
+            Lista.Add("Janete");
+            Lista.Add("Otávio");
+            Lista.Add("Marcelo");
+            Lista.Add("Pedro");
+            Lista.Add("Thais");
+            Lista.Remove("Otávio");
+            string auxiliar = "";
+            foreach (string nome in Lista)
+            {
+                auxiliar += nome + "n";
+            }
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExercicio3_Click(object sender, EventArgs e)
+        {
+            double[,] notas = new double[20, 3];
+            double[] media = new double[20];
+            string auxiliar, auxiliar2 = " Aluno
+            Nota 1 Nota 2 Nota 3\n *;
+            for (int i = 0;
+            i ‹ notas.GetLength(0); i++)
+            for (int j = 0; j ‹ notas.GetLength(1); j +)
+            auxiliar = Interaction.InputBox($"Digite a nota (j+1) do aluno (i+1)");
+            if (!double.TryParse(auxiliar, out notas(i, j)))
+                MessageBox.Show("Número inválido");
+            else
+            if (notas(i, j] ‹ 0 Il notas(i, j] > 10)
+            MessageBox.Show("Notas devem estar entre 0 e 10");
+            i -;
+            if (i ‹ 9)
+            auxiliar2 += ($"Aluno Oi + 1}
+            " + notas(i, 0]+ "
+            " + notas(i, 0] + "
+            else
+                auxiliar2 += ($"Aluno (1 + 1}
+            for (int 1 = 0; 1 < media.GetLength(0); 1H)
+                media[i] = (notas[i, 0] + notas(i, 1] + notas(1, 21) / 3;
+            auxiliar =
+            for (int i = 0; 1 < media.GetLength(e); i + *)
+                auxiliar #= (S"Aluno (I+ 1): média: " + medialij. Tostring("129) + Ia);
+            MessageBox.Show(auxiliar);
+            MessageBox.Show(auxiliar2);
+        }
+    }
+}
